74d043bc41c370fd64252653fb2630bac043aa03
Rodrig087 <miltonho@hotmail.com>
2023-11-15 12:58:08 -0500

    En proceso: Se esta modificando el programa para procesar todas las fotos de una estacion en W10.

--- Sofware/W10/CalculoCoordinometro.py
+++ Sofware/W10/CalculoCoordinometro.py
@@ -8,7 +8,7 @@ import time
 
 # ////////////////////////////////// Metodos //////////////////////////////////
 
-def ProcesarImagen(pathImagen, pathDatos):
+def ProcesarImagen(Imagen, pathImagen, pathDatos):
        
        mat_datos_x=[]
        puntoMenor=0
@@ -20,39 +20,58 @@ def ProcesarImagen(pathImagen, pathDatos):
        global contadorArchivosProcesados
        global contadorArchivosSinProcesar
        
+       '''
+       tiempoImagen = Imagen[0:19]  
+       tiempoImagen_dt = datetime.strptime(tiempoImagen, '%Y-%m-%d %H_%M_%S')
+       tiempoImagen_formateado = tiempoImagen_dt.strftime('%Y-%m-%dT%H:%M:%S')
+       print(tiempoImagen_formateado)
+       #img = cv2.imread(pathImagen+Imagen)
+       '''
        try:
        ####################-Procesamiento de Imagen-##########################
        #######################################################################
        
-              tiempoImagen = pathImagen[54:73]  
+              tiempoImagen = Imagen[0:19]  
               tiempoImagen_dt = datetime.strptime(tiempoImagen, '%Y-%m-%d %H_%M_%S')
-              tiempoImagen_str = str(tiempoImagen_dt)
+              tiempoImagen_str = tiempoImagen_dt.strftime('%Y-%m-%dT%H:%M:%S')
+              print(tiempoImagen_str)
        
-              img = cv2.imread(pathImagen)
+              pathFoto =  pathImagen+Imagen      
+       
+              img = cv2.imread(pathFoto)
               height, width = img.shape[:2]
                 
               gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
               gauss = cv2.medianBlur(gris,13)
-              #gauss = cv2.GaussianBlur(gris, (3,3), 0)
-              #cv2.imwrite(pathDatos[0:43]+'filtroblur.jpg',gauss)
-              
-              canny = cv2.Canny(gauss, 100, 150)
-              #canny = cv2.Canny(gauss, 10, 50)
-              #canny = cv2.Canny(gauss, 5, 10)
                             
+              canny = cv2.Canny(gauss, 20, 60)
+                                         
               lines = cv2.HoughLinesP(image=canny,rho=1,theta=np.pi/180, threshold=100,lines=np.array([]), minLineLength=180,maxLineGap=100)
                      
               a,b,c = lines.shape
 
-              f = open(pathDatos,"a")
+              #f = open(pathDatos,"a")
 
               for i in range(a):
-                     cv2.line(gauss, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 0, 255), 3, cv2.LINE_AA)
-                     #print(lines[i][0][0], lines[i][0][1], lines[i][0][2], lines[i][0][3])
-                     mat_datos_x.append(lines[i][0][0])
-                     #cv2.imwrite(direccionCarpeta+'lineasMarcadas.jpg',gauss)
-                     #cv2.imwrite(pathDatos[0:43]+'lineasMarcadas.jpg',gauss)
-
+                  '''   
+                  cv2.line(gauss, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 0, 255), 3, cv2.LINE_AA)
+                  #print(lines[i][0][0], lines[i][0][1], lines[i][0][2], lines[i][0][3])
+                  mat_datos_x.append(lines[i][0][0])
+                  #cv2.imwrite(direccionCarpeta+'lineasMarcadas.jpg',gauss)
+                  #cv2.imwrite(pathDatos[0:43]+'lineasMarcadas.jpg',gauss)
+                  '''
+                  cv2.line(gauss, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 255, 0), 3, cv2.LINE_AA)
+                  mat_datos_x.append(lines[i][0][0])
+                  cv2.imwrite(pathDatos+'lineasMarcadas.jpg',gauss)
+              
+              puntoMayor = max(mat_datos_x)
+              puntoMenor = min(mat_datos_x)
+              
+              
+              
+              print(puntoMayor + puntoMenor)
+              
+              '''
               puntoMayor = max(mat_datos_x)
               puntoMenor = min(mat_datos_x)
               #anchoCable=215
@@ -86,12 +105,61 @@ def ProcesarImagen(pathImagen, pathDatos):
               puntoMenor=0
               puntoMayor=0
               
+              '''
+              
        except:
            
               print("No se pudo realizar el calculo")
               contadorArchivosSinProcesar = contadorArchivosSinProcesar + 1
+       
               
  # /////////////////////////////////////////////////////////////////////////////
+
+
+
+def Procesar(Imagen, pathImagen, pathDatos):
+       
+       mat_datos_x=[]
+       puntoMenor=0
+       
+       global bordeMenorAnterior
+       global desfaceAnterior
+       global contadorErrores
+       global contadorArchivosProcesados
+       global contadorArchivosSinProcesar
+       
+             
+       tiempoImagen = Imagen[0:19]  
+       tiempoImagen_dt = datetime.strptime(tiempoImagen, '%Y-%m-%d %H_%M_%S')
+       tiempoImagen_str = tiempoImagen_dt.strftime('%Y-%m-%dT%H:%M:%S')
+       #print(tiempoImagen_str)
+        
+       img = cv2.imread(pathImagen+Imagen)
+       height, width = img.shape[:2]
+       
+                             
+       gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+       gauss = cv2.medianBlur(gris,13)
+       cv2.imwrite(pathDatos+'filtroblur.jpg',gauss)
+       canny = cv2.Canny(gauss, 10, 30)
+       
+       print('llego')
+                        
+       lines = cv2.HoughLinesP(image=canny,rho=1,theta=np.pi/180, threshold=100,lines=np.array([]), minLineLength=180,maxLineGap=100)
+       a,b,c = lines.shape
+
+        #f = open(pathDatos,"a")
+
+       for i in range(a):
+           cv2.line(gauss, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 255, 0), 3, cv2.LINE_AA)
+           mat_datos_x.append(lines[i][0][0])
+           cv2.imwrite(pathDatos+'lineasMarcadas.jpg',gauss)
+        
+       puntoMayor = max(mat_datos_x)
+       puntoMenor = min(mat_datos_x)
+        
+       print(str(tiempoImagen_str) + ' ' + str(puntoMayor) + ' ' + str(puntoMenor))
+
 
 
 
@@ -100,42 +168,35 @@ def ProcesarImagen(pathImagen, pathDatos):
 if __name__ == '__main__':
     
     #******************************************************************************
-    #Obtiene el nombre de todos los archivos:
-    #Lista los archivos del directorio a subir
-    nombreEje = "EJE2"
-    nombreDispositivo = "N1_X"
-    nombreArchivoMediciones = nombreEje + nombreDispositivo + ".txt"
-    rutaArchivos = "C:/Users/milto/Desktop/Coordinometros/"+nombreEje+"/Fotos/" + nombreDispositivo + "/"
-    print(rutaArchivos)
-    listaArchivos = os.listdir(rutaArchivos)
+    # Directorio donde estan las fotos a procesar y directorio donde se guardara el archivo de respuesta
+    directorioFotos= 'C:/Users/milto/Milton/RSA/Proyectos/Proyecto Chanlud/Analisis/Coordinometros/Fotos/IZQ/IZQ-N2-X/2023/'
+    directorioDatos = 'C:/Users/milto/Milton/RSA/Proyectos/Proyecto Chanlud/Analisis/Coordinometros/Fotos/IZQ/IZQ-N2-X/'
+    #******************************************************************************
+    #Obtiene el nombre de todas las fotos del directorio:
+    listaArchivos = os.listdir(directorioFotos)
     listaArchivosOrdenada = sorted(listaArchivos) 
-    listaArchivosOrdenada = listaArchivosOrdenada[0:(len(listaArchivosOrdenada)-3)]
-    #print(listaArchivosOrdenada)    
+    listaArchivosOrdenada = listaArchivosOrdenada[0:(len(listaArchivosOrdenada))]
+    #print(listaArchivosOrdenada)
     #******************************************************************************
-    
-    #******************************************************************************
-    #Lee o crea el archivo de mediciones:
-    pathArchivoMediciones = rutaArchivos[0:43]+nombreArchivoMediciones        
-    #archivoMediciones = open(pathArchivoMediciones,"a")
-    pathDatos = pathArchivoMediciones
-    #******************************************************************************
-    
+    # Procesa las fotos una por una    
     bordeMenorAnterior = 0
     desfaceAnterior = 0
     contadorArchivosProcesados = 0
     contadorErrores = 0
     contadorArchivosSinProcesar = 0
     
-    for nombreImagen in listaArchivosOrdenada:
-        
-        pathImagen = rutaArchivos + nombreImagen   
-        ProcesarImagen(pathImagen, pathDatos)
+    for foto in listaArchivosOrdenada:
+        Procesar(foto, directorioFotos, directorioDatos)
         
     #archivoMediciones.close()
-    
+        
     print("Numero total de archivos: %d" %(len(listaArchivosOrdenada))) 
     print("Numero de archivos procesados: %d" %contadorArchivosProcesados)
     print("Numero de archivos sin procesar: %d" %contadorArchivosSinProcesar)
     print("Numero de archivos con errores: %d" %contadorErrores)
+    
+    print(listaArchivosOrdenada[0][0:19])
+    
+    #******************************************************************************
 
 # /////////////////////////////////////////////////////////////////////////////            
--- Sofware/W10/DetectorImagen.py
+++ Sofware/W10/DetectorImagen.py
@@ -3,9 +3,6 @@ import cv2
 from datetime import date
 from datetime import datetime
 import os
-
-
-import os
 import time
 
 
@@ -16,56 +13,50 @@ import time
 
 def ProcesarImagen(imagen, direccionCarpeta):
        
-       mat_datos_x=[]
-       puntoMenor=0
+              mat_datos_x=[]
+              puntoMenor=0
        
-       try:
+       #try:
        ####################-Procesamiento de Imagen-##########################
        #######################################################################
               
+              '''
               tiempoImagen = imagen[54:73]  
               tiempoImagen_dt = datetime.strptime(tiempoImagen, '%Y-%m-%d %H_%M_%S')
               tiempoImagen_str = str(tiempoImagen_dt)
               print(tiempoImagen)
               print(tiempoImagen_dt)
+             '''
               
               img = cv2.imread(imagen)
               height, width = img.shape[:2]
                            
               gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
               gauss = cv2.medianBlur(gris,13)
-              #gauss = cv2.GaussianBlur(gris, (5,5), 0)
-              #gauss = cv2.GaussianBlur(gris, (31,31), 0)
               cv2.imwrite(direccionCarpeta+'filtroblur.jpg',gauss)
-              #canny = cv2.Canny(gauss, 100, 150)
-              canny = cv2.Canny(gauss, 5, 10)
+              canny = cv2.Canny(gauss, 10, 30)
                             
               lines = cv2.HoughLinesP(image=canny,rho=1,theta=np.pi/180, threshold=100,lines=np.array([]), minLineLength=180,maxLineGap=100)
-                     
               a,b,c = lines.shape
-              #print("llego")
-              #f = open(direccionCarpeta+"datosMedidos.txt","a")
-
+              
               for i in range(a):
                      cv2.line(gauss, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 255, 0), 3, cv2.LINE_AA)
-                     #print(lines[i][0][0], lines[i][0][1], lines[i][0][2], lines[i][0][3])
                      mat_datos_x.append(lines[i][0][0])
                      cv2.imwrite(direccionCarpeta+'lineasMarcadas.jpg',gauss)
 
               puntoMayor = max(mat_datos_x)
               puntoMenor = min(mat_datos_x)
-              #anchoCable=215
               anchoCable = puntoMayor - puntoMenor
               relacionPixel = 2/anchoCable
               bordeMenor = puntoMenor*relacionPixel
               
-              # print("Borde menor [px]: %d" % puntoMenor)
-              # print("Borde mayor [px]: %d" % puntoMayor)
-              # print("Grosor del cable [px]: %d" % anchoCable)
-              # print("Relacion mm/px: %f" % relacionPixel)
-              # print("Posicion borde menor [mm]: %f" % (puntoMenor*relacionPixel))
+              print("Borde menor [px]: %d" % puntoMenor)
+              print("Borde mayor [px]: %d" % puntoMayor)
+              print("Grosor del cable [px]: %d" % anchoCable)
+              print("Relacion mm/px: %f" % relacionPixel)
+              print("Posicion borde menor [mm]: %f" % (puntoMenor*relacionPixel))
               
-              print( tiempoImagen_str + "\t%d\t%d\t%d\t%.3f\n" % (puntoMenor,puntoMayor,anchoCable,bordeMenor))
+              #print( tiempoImagen_str + "\t%d\t%d\t%d\t%.3f\n" % (puntoMenor,puntoMayor,anchoCable,bordeMenor))
               
               mat_datos_x.clear()
 
@@ -75,19 +66,18 @@ def ProcesarImagen(imagen, direccionCarpeta):
               
               puntoMenor=0
               puntoMayor=0
-       except:
-              print("Imagen Mala para realizar el calculo")
+       #except:
+        #      print("Imagen Mala para realizar el calculo")
               
 #******************************************************************************
 
 
 #************************************ Main ************************************
 
-#direccionCarpeta = "D:/Proyecto Chanlud/Analisis/Coordinometros/fotos/"
-#direccionCarpeta = "C:/Users/milto/Milton/RSA/Proyectos/Proyecto Chanlud/Analisis/Coordinometros/Fotos/"
-direccionCarpeta = "C:/Users/milto/Desktop/Coordinometros/EJE2/Fotos/N1_X/"
+direccionCarpeta = 'C:/Users/milto/Milton/RSA/Proyectos/Proyecto Chanlud/Analisis/Coordinometros/Fotos/prueba/'
 
-nombreArchivo = input("Ingrese el nombre de la foto: ")
+#nombreArchivo = input("Ingrese el nombre de la foto: ")
+nombreArchivo = '2023-04-13 11_44_11.712927' 
 
 imagen = direccionCarpeta + nombreArchivo + ".jpg"
 #imagen = "Foto1.jpg"
